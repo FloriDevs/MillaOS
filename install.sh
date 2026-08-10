090576242ac8577145c71b40b9ec8e3e5a3b6c3d
FloriDevs <penguinflo12@outlook.com>
2026-08-10 14:04:22 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+echo "installing iso tools, grub and grub-efi using apt"
+sudo apt install xorriso grub-pc-bin grub-efi-amd64-bin
+echo "installing build-essential using apt"
+sudo apt install build-essential
+echo "installing nasm using apt"
+sudo apt install nasm
